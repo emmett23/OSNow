e60254be304aa14e3a193542ffa47d307c3778fa
Emase <corneliuslochipi@gmail.com>
2024-06-17 16:12:59 +0300

    update: footer

--- src/app/_components/sections/Footer.tsx
+++ src/app/_components/sections/Footer.tsx
@@ -10,90 +10,94 @@ import { Tooltip } from "@mantine/core";
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <footer className="flex h-full min-h-[18vh] w-full flex-col items-center justify-center bg-gray-300 p-4 text-black md:p-8">
-      <h1 className="mb-4 text-lg font-bold text-blue-700 md:text-2xl">
-        Open Source Now
-      </h1>
-      <ul className="mb-4 flex flex-col items-center gap-2 md:flex-row">
-        <li>
-          <Link href="/faqs">
-            <p className="hover:underline">FAQS</p>
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact">
-            <p className="hover:underline">Contact</p>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <p className="hover:underline">About</p>
-          </Link>
-        </li>
-      </ul>
-      <div className="mb-4 flex gap-3">
-        <Link
-          href="https://twitter.com"
-          aria-label="Twitter"
-          className="rounded-full border-2 border-gray-400 p-1 hover:text-blue-500 md:p-2"
-        >
-          <FaXTwitter className="text-xl md:text-2xl" />
-        </Link>
-        <Link
-          href="https://facebook.com"
-          aria-label="Facebook"
-          className="rounded-full border-2 border-gray-400 p-1 hover:text-blue-700 md:p-2"
-        >
-          <FaFacebookF className="text-xl md:text-2xl" />
-        </Link>
-        <Link
-          href="https://linkedin.com"
-          aria-label="LinkedIn"
-          className="rounded-full border-2 border-gray-400 p-1 hover:text-blue-800 md:p-2"
-        >
-          <FaLinkedinIn className="text-xl md:text-2xl" />
-        </Link>
-        <Link
-          href="https://instagram.com"
-          aria-label="Instagram"
-          className="rounded-full border-2 border-gray-400 p-1 hover:text-pink-500 md:p-2"
-        >
-          <FaInstagram className="text-xl md:text-2xl" />
-        </Link>
-      </div>
-      <div className="text-center text-sm md:text-base">
-        <p className="mb-2">
-          Built by the team, including you! Awaiting your
-          <Tooltip
-            label="Pull Requests - your changes to the codebase."
-            position="top"
-            withArrow
-            className="inline-block"
-          >
-            <Link
-              href="https://github.com/emmett23"
-              className="pl-1 font-semibold text-blue-900 hover:underline"
-            >
-              PR
+    <div className="h-full min-h-[18vh] w-full bg-[#FFFCE1] px-2 text-gray-800 md:px-4">
+      <footer className="flex flex-col items-center justify-center p-4 md:p-8">
+        <h1 className="mb-4 text-lg font-bold text-blue-700 md:text-2xl">
+          Open Source Now
+        </h1>
+        <ul className="mb-4 flex flex-col items-center gap-2 md:flex-row">
+          <li>
+            <Link href="/faqs">
+              <p className="text-sm hover:underline">FAQS</p>
             </Link>
-          </Tooltip>
-        </p>
-        <p className="mb-2">Copyright OSNow, {year}</p>
-        <p className="mb-2">
+          </li>
+          <li>
+            <Link href="/contact">
+              <p className="text-sm hover:underline">Contact</p>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <p className="text-sm hover:underline">About</p>
+            </Link>
+          </li>
+        </ul>
+        <div className="mb-4 flex gap-3">
+          <Link
+            href="https://twitter.com"
+            aria-label="Twitter"
+            className="rounded-full border-2 border-gray-400 p-1 hover:text-blue-500 md:p-2"
+          >
+            <FaXTwitter className="text-xl md:text-2xl" />
+          </Link>
+          <Link
+            href="https://facebook.com"
+            aria-label="Facebook"
+            className="rounded-full border-2 border-gray-400 p-1 hover:text-blue-700 md:p-2"
+          >
+            <FaFacebookF className="text-xl md:text-2xl" />
+          </Link>
+          <Link
+            href="https://linkedin.com"
+            aria-label="LinkedIn"
+            className="rounded-full border-2 border-gray-400 p-1 hover:text-blue-800 md:p-2"
+          >
+            <FaLinkedinIn className="text-xl md:text-2xl" />
+          </Link>
+          <Link
+            href="https://instagram.com"
+            aria-label="Instagram"
+            className="rounded-full border-2 border-gray-400 p-1 hover:text-pink-500 md:p-2"
+          >
+            <FaInstagram className="text-xl md:text-2xl" />
+          </Link>
+        </div>
+        <div className="text-center text-sm md:text-base">
+          <p className="mb-2">
+            Built by the team, including you! Awaiting your
+            <Tooltip
+              label="Pull Requests - your changes to the codebase."
+              position="top"
+              withArrow
+              className="inline-block"
+            >
+              <Link
+                href="https://github.com/emmett23"
+                className="pl-1 font-semibold text-blue-900 hover:underline"
+              >
+                PR
+              </Link>
+            </Tooltip>
+          </p>
+        </div>
+      </footer>
+      <div className="flex flex-col items-center border-t border-gray-500 md:flex-row md:justify-between">
+        <p className="mb-2 md:mb-0">Copyright <span className="text-blue-800 font-bold">OSNow</span>, {year}</p>
+        <p className="mb-2 md:mb-0">
           <Link href="/terms" className="hover:underline">
             Terms and Conditions
           </Link>
-          |
+          {" | "}
           <Link href="/privacy" className="hover:underline">
             Privacy
           </Link>
-          |
+          {" | "}
           <Link href="/legal" className="hover:underline">
             Legal notice
           </Link>
         </p>
       </div>
-    </footer>
+    </div>
   );
 };
 

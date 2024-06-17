c8e2c392aa2b4771e0b4f4ece0893fdf50633d22
Emase <corneliuslochipi@gmail.com>
2024-06-17 22:33:55 +0300

    added img hostname

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "via.placeholder.com" }],
+  },
+};
 
 export default config;

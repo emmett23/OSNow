8133d114d90cd6a06ae9fdfcffa1a8866768e41b
Emase <corneliuslochipi@gmail.com>
2024-06-17 16:16:19 +0300

    add newsletter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import { Button } from "@mantine/core";
 import Hero from "./_components/sections/Hero";
 import Challenges from "./_components/sections/Challenges";
 import HowWeWork from "./_components/sections/How-we-work";
-import Partners from "./_components/sections/Partners";
-import Subscribe from "./_components/sections/Subscribe";
+import Partners from "./_components/sections/Partners"; 
 import Footer from "./_components/sections/Footer";
+import NewsLetter from "./_components/sections/NewsLetter";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
+    <main className="flex min-h-screen flex-col items-center bg-gray-200">
       <p>More coming soon</p>
 
       <Link href="https://github.com/emmett23" target="_blank">
@@ -19,8 +19,8 @@ export default function HomePage() {
       <Hero />
       <Challenges />
       <HowWeWork />
-      <Partners />
-      <Subscribe />
+      <Partners /> 
+      <NewsLetter />
       <Footer />
     </main>
   );

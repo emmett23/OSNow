ed31f041b894b64a67c0086e2bf111f306d6bbcc
Emase <corneliuslochipi@gmail.com>
2024-06-28 16:11:25 +0300

    added how we work section

--- src/app/partner-with-us/page.tsx
+++ src/app/partner-with-us/page.tsx
@@ -1,11 +1,74 @@
+"use client";
+
 import Container from "../_components/custom-components/container";
+import { Grid, Image, Title, Text } from "@mantine/core";
+
+const partners = [
+  "https://via.placeholder.com/70?text=Walmart",
+  "https://via.placeholder.com/70?text=Whirlpool",
+  "https://via.placeholder.com/70?text=Electrolux",
+  "https://via.placeholder.com/70?text=Novolex",
+  "https://via.placeholder.com/70?text=Macy's",
+  "https://via.placeholder.com/70?text=PaperWorks",
+  "https://via.placeholder.com/70?text=Hendrickson",
+  "https://via.placeholder.com/70?text=Walmart",
+  "https://via.placeholder.com/70?text=Whirlpool",
+  "https://via.placeholder.com/70?text=Electrolux",
+  "https://via.placeholder.com/70?text=Novolex",
+  "https://via.placeholder.com/70?text=Macy's",
+];
 
 const page = () => {
   return (
-    <Container className="pt-[120px] min-h-screen">
-      <h1 className="text-4xl font-extrabold tracking-normal text-gray-700 sm:text-5xl lg:text-6xl">
-        Be our partner
-      </h1>
+    <Container className="min-h-screen pt-[120px] text-gray-800">
+      <Title mt="xl" mb="md">
+        We work with these partners
+      </Title>
+      <Grid justify="center" align="center">
+        {partners.slice(0, 6).map((partner, index) => (
+          <Grid.Col key={index} span={2}>
+            <Image
+              src={partner}
+              alt={`Partner ${index + 1}`}
+              width={70}
+              height={70}
+            />
+          </Grid.Col>
+        ))}
+        {partners.slice(6, 12).map((partner, index) => (
+          <Grid.Col key={index} span={2}>
+            <Image
+              src={partner}
+              alt={`Partner ${index + 7}`}
+              width={70}
+              height={70}
+            />
+          </Grid.Col>
+        ))}
+      </Grid>
+
+      <div className="py-4">
+        <Title className="text-5xl" mt="xl" mb="md">
+          Accelerate your professional <br /> growth with <br /> Open Source Now
+        </Title>
+        <div className="flex items-center gap-2">
+          <Text className="text-center" mb="xl">
+            We work with
+          </Text>
+          <Grid justify="center" align="center">
+            {partners.slice(0, 6).map((partner, index) => (
+              <Grid.Col key={index} span={2}>
+                <Image
+                  src={partner}
+                  alt={`Partner ${index + 1}`}
+                  width={5}
+                  height={5}
+                />
+              </Grid.Col>
+            ))}
+          </Grid>
+        </div>
+      </div>
     </Container>
   );
 };
